e14e9ae2bc7a2c55d7b40bf8d954679c41b025fb
“vstevenlab” <labsteven19@gmail.com>
2023-05-17 11:41:01 -0400

    nat-gateway file

---
+++ nat-gateway/variables.tf
@@ -0,0 +1,12 @@
+# nat-gateway Variables
+variable "region" {}
+variable "project_name" {}
+variable "environment" {}
+variable "public_subnet_az1_id" {}
+variable "internet_gateway" {}
+variable "public_subnet_az2_id" {}
+variable "vpc_id" {}
+variable "private_app_subnet_az1_id" {}
+variable "private_data_subnet_az1_id" {}
+variable "private_app_subnet_az2_id" {}
+variable "private_data_subnet_az2_id" {}
