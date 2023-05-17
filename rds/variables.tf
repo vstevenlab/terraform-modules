362fabb6d38fc56f657c34cfa3ceca3855a6bc37
“vstevenlab” <labsteven19@gmail.com>
2023-05-17 17:54:39 -0400

    rds module

---
+++ rds/variables.tf
@@ -0,0 +1,11 @@
+# rds variable
+variable "project_name" {}
+variable "environment" {}
+variable "private_data_subnet_az1_id" {}
+variable "private_data_subnet_az2_id" {}
+variable "database_snapshot_identifier" {}
+variable "database_instance_classe" {}
+variable "availability_zone_1" {}
+variable "database_instance_identifier" {}
+variable "multi_az_deployment" {}
+variable "database_security_group_id" {}
