51e6332289d6c586d9b7c932d696f38fb80626f6
“vstevenlab” <labsteven19@gmail.com>
2023-05-18 12:25:26 -0400

    alb module

---
+++ alb/variables.tf
@@ -0,0 +1,8 @@
+variable "project_name" {}
+variable "environment" {}
+variable "alb_security_group_id" {}
+variable "public_subnet_az1_id" {}
+variable "public_subnet_az2_id" {}
+variable "target_type" {}
+variable "vpc_id" {}
+variable "certificate_arn" {}
