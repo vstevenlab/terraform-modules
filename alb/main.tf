96911e07b2ee1e0840452f4f0d8d3def1d527011
“vstevenlab” <labsteven19@gmail.com>
2023-05-18 13:12:58 -0400

    alb module

--- alb/main.tf
+++ alb/main.tf
@@ -13,7 +13,7 @@ resource "aws_lb" "application_load_balancer" {
 }
 
 # create target group
-resource "aws_lb_target_group" "alb_tg" {
+resource "aws_lb_target_group" "alb_target_group" {
   name        = "${var.project_name}-${var.environment}-tg"
   target_type = var.target_type
   port        = 80
@@ -33,7 +33,7 @@ resource "aws_lb_target_group" "alb_tg" {
 }
 
 # create a listener on port 80 with redirect action
-resource "aws_lb_listener" "alb_http" {
+resource "aws_lb_listener" "alb_http_listener" {
   load_balancer_arn = aws_lb.application_load_balancer.arn
   port              = 80
   protocol          = "HTTP"
@@ -59,7 +59,7 @@ resource "aws_lb_listener" "alb_https_listener" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.alb_tg.arn
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
   }
 }
 #alb issue
